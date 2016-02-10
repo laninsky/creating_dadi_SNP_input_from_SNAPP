c52ad97f2e2db3395087083520b04ecd21405616
Alana Alexander <laninsky@gmail.com>
2016-02-10 11:32:13 -0600

    Update fastsimcoalmodelfit.R

--- processing_fastsimcoal/fastsimcoalmodelfit.R
+++ processing_fastsimcoal/fastsimcoalmodelfit.R
@@ -1,10 +1,6 @@
-#fastsimcoalmodelfit <- function(working_dir,dadi_spectrum,fastsimcoal_def_file,fastsimcoal_MSFS_file,no_sims) {
+#You'll need to change the way the number of parameters in your data are calculated (line 34 onwards) as this will be specific to your dataset
 
-working_dir <- "C:/Users/Alana/Dropbox/ceyx/dadi/runs"
-dadi_spectrum <- "spectrum_output.txt"
-fastsimcoal_def_file <- "scenario.def"
-fastsimcoal_MSFS_file <- "scenario_MSFS.obs"
-no_sims <- 5
+fastsimcoalmodelfit <- function(working_dir,dadi_spectrum,fastsimcoal_def_file,fastsimcoal_MSFS_file,no_sims) {
 
 library(stringr)
 
@@ -12,7 +8,7 @@ setwd(working_dir)
 
 spectrum <- readLines(dadi_spectrum)
 spectrum <- unlist(strsplit(spectrum[2]," "))
-
+number_of_cats <- length(spectrum)
 output <- as.matrix(read.table(fastsimcoal_def_file))
 
 no_scenarios <- dim(output)[1]-1
@@ -30,16 +26,41 @@ AdjsRs <- cbind(AdjsRs, paste("AdjR_",i,sep=""))
 RMSE <- cbind(RMSE, paste("RMSE_",i,sep=""))
 }
 
-toadd <- matrix(ncol=15,nrow=(no_scenarios+1))
-toadd[1,] <- c(Rs,AdjsRs,RMSE)
+toadd <- matrix(ncol=16,nrow=(no_scenarios+1))
+toadd[1,] <- c("params",Rs,AdjsRs,RMSE)
 
 output <- cbind(output,toadd)
 
+for (i in 2:(no_scenarios+1)) {
+x <- 0
+if(!(output[i,6]==0)) {
+x <- 1
+}
+if(!(output[i,7]==1)) {
+x <- x + 1
+}
+output[i,beginning_output] <- 7 + x
+}
+
 input <- readLines(fastsimcoal_MSFS_file)
 
-x <- 2
+x <- 1
+i <- 1
+while (i < length(input)) {
+if (grepl("observations",input[i],fixed=TRUE)) {
+x <- x + 1
+for (j in 1:no_sims) {
+temp <- unlist(strsplit(input[i+j+1],"\t"))
+fit <- lm(as.numeric(spectrum) ~ as.numeric(temp))
+output[x, beginning_output+j] <- summary(fit)$r.squared
+output[x, (beginning_output+j+no_sims)] <- 1- (((1-summary(fit)$r.squared)*(number_of_cats-1))/(number_of_cats-as.numeric(output[x,beginning_output])-1))
+output[x, (beginning_output+j+(2*no_sims))] <- mean((fit$residuals)^2)
+}
+}
+i <- i + 1
+}
 
-for (i in 1:no_scenarios) {
-
+write.table(output,"summarize_fastsimcoal.txt", sep="\t",quote=FALSE, row.names=FALSE,col.names=FALSE)
+}
 
 
