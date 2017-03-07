414013a9f1ed351298d36bbeefc3243f301ccecd
Alana Alexander <laninsky@gmail.com>
2017-03-07 15:27:19 -0600

    Update fs_downproject.py

--- fs_downproject/fs_downproject.py
+++ fs_downproject/fs_downproject.py
@@ -9,7 +9,18 @@ with open('file_name', 'r') as myfile:
 dd = dadi.Misc.make_data_dict(dadi_input)
 
 with open(dadi_input, 'r') as myfile:
-    headerline=myfile.readline().replace('\n', '')
+    headerline=myfile.readline().replace('\n', '').split('\t')
+
+newheader = list()
+
+for i in range(len(headerline)):
+        if not ((headerline[i]=='ingroup') or (headerline[i]=='outgroup') or (headerline[i]=='Allele1') or (headerline[i]=='Allele2')):
+            if newheader==[]:
+                newheader = list(headerline[i].split)
+                delimiter=''
+                newheader = delimiter.join(newheader)
+            else:
+                newheader.append(headerline[i])
 
 final_fs = 0
 final spectrum = 0
