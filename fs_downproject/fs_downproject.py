2fc1328fd7ca0abee7a2780d94a9606669d67921
Alana Alexander <laninsky@gmail.com>
2017-03-07 19:16:14 -0600

    Update fs_downproject.py

--- fs_downproject/fs_downproject.py
+++ fs_downproject/fs_downproject.py
@@ -11,20 +11,24 @@ dd = dadi.Misc.make_data_dict(dadi_input)
 with open(dadi_input, 'r') as myfile:
     headerline=myfile.readline().replace('\n', '').split('\t')
 
-newheader = list()
+headerline.remove('ingroup')
+headerline.remove('outgroup')
+headerline.remove('Allele1')
+headerline.remove('Allele2')
 
-for i in range(len(headerline)):
-        if not ((headerline[i]=='ingroup') or (headerline[i]=='outgroup') or (headerline[i]=='Allele1') or (headerline[i]=='Allele2')):
-            if newheader==[]:
-                newheader = list(headerline[i].split)
-                delimiter=''
-                newheader = delimiter.join(newheader)
-            else:
-                newheader.append(headerline[i])
+no_of_pops = (len(headerline)-2)/2
+pop_name = headerline[0:no_of_pops]
 
 final_fs = 0
-final spectrum = 0
+final_spectrum = 0
 
-fs = dadi.Spectrum.from_data_dict(dd , pop_ids =['Pop1',    'Pop2', 'Pop3', 'Pop4'] ,projections =[6 , 22, 2, 2] ,polarized = False)
 
-fs.to_file("spectrum_output.txt")
+
+
+fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =[6 , 22, 2, 2,3] ,polarized = False)
+if fs.S() > final_fs:
+    final_fs=fs.S()
+    final_spectrum = fs
+
+
+final_spectrum.to_file("spectrum_output.txt")
