3b92fd68d9ee2b538cc77f2ad5112bb3bc83cc79
Alana Alexander <laninsky@gmail.com>
2017-03-07 19:39:26 -0600

    Update fs_downproject.py

--- fs_downproject/fs_downproject.py
+++ fs_downproject/fs_downproject.py
@@ -21,14 +21,24 @@ pop_name = headerline[0:no_of_pops]
 
 final_fs = 0
 final_spectrum = 0
+pop_projections = 0
 
+with open('fs_combos.txt', 'r') as myfile:
+    for line in myfile:
+        line=line.replace('\n', '').split(' ')
+        line = [int(i) for i in line]
 
-
-
-fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =[6 , 22, 2, 2,3] ,polarized = False)
-if fs.S() > final_fs:
-    final_fs=fs.S()
-    final_spectrum = fs
-
+        fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =line ,polarized = False)
+        if fs.S() > final_fs:
+            final_fs=fs.S()
+            final_spectrum = fs
+            pop_projections=line
 
 final_spectrum.to_file("spectrum_output.txt")
+print 'The optimum fs.S. is'
+print final_fs
+print 'The projection for this optimum is'
+print pop_name
+print pop_projections
+print 'The spectrum for this is saved as "spectrum_output.txt"'
+
