53eddcce703665d87ab761111b38a03249766f06
Alana Alexander <laninsky@gmail.com>
2017-04-03 21:18:13 -0500

    Update fs_downproject.R

--- fs_downproject/fs_downproject.R
+++ fs_downproject/fs_downproject.R
@@ -7,12 +7,5 @@ for (i in 1:no_of_pops) {
 maxcounts[1,i] <- max(max(as.numeric(file[2:(dim(file)[1]),(i+3)])),max(as.numeric(file[2:(dim(file)[1]),(i+4+no_of_pops)])))
 }
 
-array_expression <- vector(mode="list", length=no_of_pops)
-for (i in 1:no_of_pops) {
-  array_expression[[i]] <- c(1:maxcounts[1,i])
-}
-
-pop_combinations <- expand.grid(array_expression)
-
-write.table(pop_combinations,"fs_combos.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(maxcounts,"maxcount.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
   
