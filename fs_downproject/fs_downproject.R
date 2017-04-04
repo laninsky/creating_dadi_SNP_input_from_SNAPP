e1bdd92ccf96e35c621b013762ebaeb6b33c7a26
Alana Alexander <laninsky@gmail.com>
2017-04-04 13:19:55 -0500

    Update fs_downproject.R

--- fs_downproject/fs_downproject.R
+++ fs_downproject/fs_downproject.R
@@ -4,7 +4,14 @@ no_of_pops <- (length(file[1,])-6)/2
 
 maxcounts <- matrix(NA,ncol=no_of_pops,nrow=1)
 for (i in 1:no_of_pops) {
-maxcounts[1,i] <- max(max(as.numeric(file[2:(dim(file)[1]),(i+3)])),max(as.numeric(file[2:(dim(file)[1]),(i+4+no_of_pops)])))
+  for (j in 2:(dim(file)[1])) {
+    if(is.na(maxcounts[1,i])) {
+    maxcounts[1,i] <- sum(as.numeric(file[j,(i+3)]),as.numeric(file[j,(i+4+no_of_pops)]))
+    }
+    if(maxcounts[1,i] < sum(as.numeric(file[j,(i+3)]),as.numeric(file[j,(i+4+no_of_pops)]))) {
+     maxcounts[1,i] <- sum(as.numeric(file[j,(i+3)]),as.numeric(file[j,(i+4+no_of_pops)]))
+    }
+  }
 }
 
 write.table(maxcounts,"maxcount.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
