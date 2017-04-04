cf50388d2ff96edc86de0cdabb67cdac01908079
Alana Alexander <laninsky@gmail.com>
2017-04-04 12:10:50 -0500

    Update fs_downproject.py

--- fs_downproject/fs_downproject.py
+++ fs_downproject/fs_downproject.py
@@ -45,13 +45,26 @@ for i in range(0,no_of_pops):
         temp_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =temp_first_line ,polarized = False)        
         temp_fs_S = temp_fs.S()
                 
-#Do another run increasing the allele sample sizes starting from half the original sample size, if the above loop didn't down project at all. Jumping up in steps of 10, unless we are close to the max sample size
-if max_pop_projections == first_line:
-    first_line = [i/2 for i in first_line]
-    lil_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =first_line ,polarized = False)
-    for i in range(0,no_of_pops):
-        new_fs=lil_fs.S()
-        temp_first_line = first_line[:]
+#Do another run increasing the allele sample sizes starting from n=1 for all samples to see if we converge on the same outcome
+first_line = [1 for i in first_line]
+lil_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =first_line ,polarized = False)
+for i in range(0,no_of_pops):
+    new_fs=lil_fs.S()
+    temp_first_line = first_line[:]
+    if (temp_first_line[i]+10) <= max_pop_projections[i]:
+        temp_first_line[i] = temp_first_line[i]+10            
+    elif (temp_first_line[i]+5) <= max_pop_projections[i]:
+        temp_first_line[i] = temp_first_line[i]+5
+    elif (temp_first_line[i]+2) <= max_pop_projections[i]:
+        temp_first_line[i] = temp_first_line[i]+2
+    elif (temp_first_line[i]+1) <= max_pop_projections[i]:
+        temp_first_line[i] = temp_first_line[i]+1
+    else:
+        continue
+    temp_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =temp_first_line ,polarized = False)        
+    while temp_fs.S() > new_fs: 
+        min_pop_projections[i] = temp_first_line[i]
+        new_fs=temp_fs.S()
         if (temp_first_line[i]+10) <= max_pop_projections[i]:
             temp_first_line[i] = temp_first_line[i]+10            
         elif (temp_first_line[i]+5) <= max_pop_projections[i]:
@@ -63,46 +76,19 @@ if max_pop_projections == first_line:
         else:
             continue
         temp_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =temp_first_line ,polarized = False)        
-        while temp_fs.S() > new_fs: 
-            min_pop_projections[i] = temp_first_line[i]
-            new_fs=temp_fs.S()
-            if (temp_first_line[i]+10) <= max_pop_projections[i]:
-                temp_first_line[i] = temp_first_line[i]+10            
-            elif (temp_first_line[i]+5) <= max_pop_projections[i]:
-                temp_first_line[i] = temp_first_line[i]+5
-            elif (temp_first_line[i]+2) <= max_pop_projections[i]:
-                temp_first_line[i] = temp_first_line[i]+2
-            elif (temp_first_line[i]+1) <= max_pop_projections[i]:
-                temp_first_line[i] = temp_first_line[i]+1
-            else:
-                continue
-            temp_fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =temp_first_line ,polarized = False)        
-            temp_fs_S = temp_fs.S()    
+        temp_fs_S = temp_fs.S()    
 
-#Or else, if the first loop did down project, checking for any further tweaks
-
-
+#If either approach (top down, bottom up) suggests down-projecting, checking for any further tweaks, otherwise writing out
+if max_pop_projections == min_pop_projections:
+      final_spectrum = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =max_pop_projections ,polarized = False)
+      final_spectrum.to_file("spectrum_output.txt")
+      final_fs = final_spectrum.S()
+      print 'The optimum fs.S. is'
+      print final_fs
+      print 'The projection for this optimum is'
+      print pop_name
+      print max_pop_projections
+      print 'The spectrum for this is saved as "spectrum_output.txt"'
 else:
     
-        
-with open('fs_combos.txt', 'r') as myfile:
-    for line in myfile:
-        line=line.replace('\n', '').split(' ')
-        line = [int(i) for i in line]
-        print x
-        x = x + 1
-        fs = dadi.Spectrum.from_data_dict(dd , pop_ids =pop_name ,projections =line ,polarized = False)
-        if fs.S() > final_fs:
-            final_fs=fs.S()
-            final_spectrum = fs
-            pop_projections=line
-            print 'Up to the following line'
-            
-final_spectrum.to_file("spectrum_output.txt")
-print 'The optimum fs.S. is'
-print final_fs
-print 'The projection for this optimum is'
-print pop_name
-print pop_projections
-print 'The spectrum for this is saved as "spectrum_output.txt"'
 
