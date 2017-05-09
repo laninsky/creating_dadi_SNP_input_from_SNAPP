eb4ab7b2cb6324f355ec52e944f3d2d4ac1479e2
Alana Alexander <laninsky@gmail.com>
2017-05-08 22:03:40 -0500

    Update switch_SNP_coding.R

--- switch_SNP_coding.R
+++ switch_SNP_coding.R
@@ -1,22 +1,32 @@
 
 setwd("C:/Users/Alana/Downloads") # Change to wherever your file is
-input <- as.matrix(read.table("md10mc4110gbs_HF_InGrp.str"))
+input <- readLines("md10mc4110gbs_HF_InGrp.str")
 
 # Replacing your 0-3 and -9 missing numbering with 1-4 and 0 for missing data
 
-for (i in 1:(dim(input)[1])) {
-temp <- which(grepl(3, input[i,3:(dim(input)[2])])==TRUE)+2
-input[i,temp] <- 4
+for (i in 3:length(input)) {
+temp <- unlist(strsplit(input[i],"\t"))
+  
+temp_replace <- which(grepl(3, temp[3:length(temp)])==TRUE)+2
+temp[temp_replace] <- 4
 
-temp <- which(grepl(2, input[i,3:(dim(input)[2])])==TRUE)+2
-input[i,temp] <- 3
+temp_replace <- which(grepl(2, temp[3:length(temp)])==TRUE)+2
+temp[temp_replace] <- 3
 
-temp <- which(grepl(1, input[i,3:(dim(input)[2])])==TRUE)+2
-input[i,temp] <- 2
+temp_replace <- which(grepl(1, temp[3:length(temp)])==TRUE)+2
+temp[temp_replace] <- 2
 
-temp <- which(grepl(0, input[i,3:(dim(input)[2])])==TRUE)+2
-input[i,temp] <- 1
+temp_replace <- which(grepl(0, temp[3:length(temp)])==TRUE)+2
+temp[temp_replace] <- 1
 
-temp <- which(grepl(-9, input[i,3:(dim(input)[2])])==TRUE)+2
-input[i,temp] <- 0
+temp_replace <- which(grepl(-9, temp[3:length(temp)])==TRUE)+2
+temp[temp_replace] <- 0
+
+temp <- paste(temp,collapse="\t")
+
+input[i] <- temp
+  
 }
+
+write.table(input,"output_0_4.str",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+
