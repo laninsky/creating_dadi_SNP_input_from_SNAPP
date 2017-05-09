dd088d08853431dc0489c5af824ef53ddbe3bdaa
Alana Alexander <laninsky@gmail.com>
2017-05-08 22:28:38 -0500

    Update add_outgroup.R

--- add_outgroup.R
+++ add_outgroup.R
@@ -1,1 +1,11 @@
+filename <- readLines("file_name")
+input_file <- readLines(filename)
 
+temp <- (unlist(strsplit(input_file[3],"\t")))
+temp[1] <- paste(temp[1],"_outgroup",sep="")
+temp[2] <- "outgroup"
+temp <- paste(temp,collapse="\t")
+
+output <- c(input_file[1:2],temp,temp,input_file[3:(length(input_file))])
+
+write.table(output,"output_outgroup.str",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
