7af1ce9deb507ac06980f30428376817ff916236
Alana Alexander <laninsky@gmail.com>
2017-05-08 21:42:14 -0500

    Update add_locusIDs.R

--- add_locusIDs.R
+++ add_locusIDs.R
@@ -1,4 +1,10 @@
 filename <- readLines("file_name")
 input_file <- readLines(filename)
 
-number_of_sites <- length(unlist(strsplit(input_file[2],"\t")))
+number_of_sites <- length(unlist(strsplit(input_file[2],"\t")))-2
+site_array <- c(1:number_of_sites)
+site_array <- paste(c("","",site_array),collapse="\t")
+
+output <- c(input_file[1],site_array,input_file[2:(length(input_file))])
+
+write.table(output,"output.str",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
